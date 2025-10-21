1cfb534c079a68481a77bbcdc002b35aea2e7d13
jacometv31 <jacomet.v31@gmail.com>
2025-10-21 18:52:13 +0200

    ok

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # script0.sh
 cd archive-19
-echo "Voici le nombre de Location pour 2016"
-grep Location 2016*.ann | wc -l
-echo "Voici le nombre de Location pour 2017"
-grep Location 2017*.ann | wc -l
-echo "Voici le nombre de Location pour 2018"
-grep Location 2018*.ann | wc -l
+for i in 2016 2017 2018
+do
+grep Location $i*.ann | wc -l
+echo "Voici le nombre de Location pour $i"
+done
 exit
