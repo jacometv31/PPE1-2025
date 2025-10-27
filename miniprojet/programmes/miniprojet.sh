086afc48ac2f08cf32991c8732d10f5716caddb1
jacometv31 <jacomet.v31@gmail.com>
2025-10-27 15:01:33 +0100

    ok

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -2,7 +2,10 @@
     i=1
     while read -r line;
     do
-        echo -e "${i}\t${line}";
+        code=$(curl -o /dev/null -s -w "%{http_code}" "$line");
+        encodage=$(curl -s -I "$line" | grep -oE "charset=[^; ]+" | cut -d= -f2)
+        mots=$(lynx -dump -nolist "$line" | wc -w)
+        echo -e "${i}\t${code}\t$mots\t${line}\t${encodage}";
         ((i++));
     done < "../urls/fr.txt";
 } >> "../tableaux/tableau-fr.tsv";
