d39f0c123558e4214553bf28bf85f500c28a1ec9
jacometv31 <jacomet.v31@gmail.com>
2025-11-10 17:06:24 +0100

    ok

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -12,23 +12,23 @@ OUTPUT_FILE="$2"
 # Début du HTML
 echo "<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset='UTF-8'>
-        <title>Résultat URLs</title> <!-- Le nom de la page est Résultat URLs -->
-        <style>
-        table { border-collapse: collapse; width: 100%; }
-        th, td { border: 1px solid #000; padding: 5px; text-align: left; }
-        </style> <!-- permet la mise en page, ici style est utilisé pour la mise en forme du tableau -->
-    </head>
+<head>
+<meta charset='UTF-8'>
+<title>Résultat URLs</title> <!-- Le nom de la page est Résultat URLs -->
+<style>
+table { border-collapse: collapse; width: 100%; }
+th, td { border: 1px solid #000; padding: 5px; text-align: left; }
+</style> <!-- permet la mise en page, ici style est utilisé pour la mise en forme du tableau -->
+</head>
 <body>
-    <table>
-    <tr>
-    <th>#</th>
-    <th>Code HTTP</th>
-    <th>Nombre de mots</th>
-    <th>URL</th>
-    <th>Encodage</th>
-    </tr>" > "$OUTPUT_FILE" #permet de créer le tableau
+<table>
+<tr>
+<th>#</th>
+<th>Code HTTP</th>
+<th>Nombre de mots</th>
+<th>URL</th>
+<th>Encodage</th>
+</tr>" > "$OUTPUT_FILE" #permet de créer le tableau
 
 # Parcours des URLs
 i=1
