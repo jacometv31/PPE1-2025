3617c888eb5a635fd044ab388365eab36fc20854
jacometv31 <jacomet.v31@gmail.com>
2025-11-10 15:09:11 +0100

    ok

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,12 +1,24 @@
-{
-    i=1
-    while read -r line;
-    do
-        code=$(curl -o /dev/null -s -w "%{http_code}" "$line");
-        encodage=$(curl -s -I "$line" | grep -oE "charset=[^; ]+" | cut -d= -f2);
-        mots=$(lynx -dump -nolist "$line" | wc -w);
-        echo -e "${i}\t${code}\t$mots\t${line}\t${encodage}";
-        ((i++));
-    done < "../urls/fr.txt";
-} >> "../tableaux/tableau-fr.tsv";
+#!/bin/bash
+
+#On affiche un message d'erreur dans le cas où on a pas nos deux arguments
+if [ $# -ne 2 ];
+then
+    echo "Erreur: il faut deux arguments pour exécuter le script"
+    exit 1
+fi
+
+#Cela nous permet de rentrer les chemins des fichiers d'entrée et de sortie lorsqu'on exécute le script
+#Cela nous évite de noter les chemins directement dans le code
+URL_FILE="$1";
+OUTPUT_FILE="$2";
+
+i=1
+while read -r line;
+do
+    code=$(curl -o /dev/null -s -w "%{http_code}" "$line");
+    encodage=$(curl -s -I "$line" | grep -oE "charset=[^; ]+" | cut -d= -f2);
+    mots=$(lynx -dump -nolist "$line" | wc -w);
+    echo -e "${i}\t${code}\t${mots}\t${line}\t${encodage}";
+    ((i++));
+done < "$URL_FILE" >> "$OUTPUT_FILE";
 
